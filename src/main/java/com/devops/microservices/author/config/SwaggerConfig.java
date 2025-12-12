53c21701e0dafda016e65cc2ff4d317544608357
Khawla Rouis <38538299+rouisskhawla@users.noreply.github.com>
2025-12-12 19:12:41 +0000

    change image tag

--- src/main/java/com/devops/microservices/author/config/SwaggerConfig.java
+++ src/main/java/com/devops/microservices/author/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.devops.microservices.authors.config;
+package com.devops.microservices.author.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
